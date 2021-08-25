8aff7456ff0d0a8595501fcb7d814a76c3c964c7
NaronB66 <narek660592@gmail.com>
2021-08-25 11:30:39 -0600

    water,URP

--- Brackeys Game Jam 2021.2/Assets/_/Work - Naron/Scripts/Playables/PlayerMove.cs
+++ Brackeys Game Jam 2021.2/Assets/_/Work - Naron/Scripts/Playables/PlayerMove.cs
@@ -218,7 +218,7 @@ namespace BGJ20212.Game.Naron
         private void Attack()
         {
             print("here");
-            if (isGrounded && canAttack && !isStanding)
+            if ( canAttack && !isStanding)
             {
                 animator.SetTrigger("Attack");
                 isAttacking = true;
