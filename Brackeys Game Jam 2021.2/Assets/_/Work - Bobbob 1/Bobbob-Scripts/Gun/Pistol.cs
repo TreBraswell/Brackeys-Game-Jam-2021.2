5462c17f6b853b0cca1f481aecc45bb12de27e06
ApprenticeGC <apprentice@giantcroissant.com>
2021-08-25 21:20:35 +0800

    Add level
    Add Mark code
    Adjust bobbob code

--- Brackeys Game Jam 2021.2/Assets/_/Work - Bobbob 1/Bobbob-Scripts/Gun/Pistol.cs
+++ Brackeys Game Jam 2021.2/Assets/_/Work - Bobbob 1/Bobbob-Scripts/Gun/Pistol.cs
@@ -1,16 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Pistol : Gun
+namespace BGJ20212.Game.Bobbob
 {
-    public override void Fire()
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Pistol : Gun
     {
-        Rigidbody instantiatedProjectile = Instantiate(projectile.GetComponent<Rigidbody>(), transform.position,transform.rotation)as Rigidbody;
+        public override void Fire()
+        {
+            Rigidbody instantiatedProjectile =
+                Instantiate(projectile.GetComponent<Rigidbody>(), transform.position, transform.rotation) as Rigidbody;
 
-        instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, 10));
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, 10));
 
-        Debug.Log("bang bang");
+            Debug.Log("bang bang");
+        }
+
     }
-
 }
