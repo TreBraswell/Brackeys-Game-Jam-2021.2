6cfba3d264a528fca0b2af937e4d02a66d9603e6
NaronB66 <narek660592@gmail.com>
2021-08-26 15:09:51 -0600

    DOORS

--- Brackeys Game Jam 2021.2/Assets/_/Work - Naron/Scripts/PlayerDoorOpen.cs
+++ Brackeys Game Jam 2021.2/Assets/_/Work - Naron/Scripts/PlayerDoorOpen.cs
@@ -9,7 +9,25 @@ namespace BGJ20212.Game.Naron
         [SerializeField] private float rayLength = 5f;
         [SerializeField] private LayerMask layerMask;
         [SerializeField] private string excluseLayeName = null;
+        GameObject raycastObject;
+        private void Update()
+        {
+            RaycastHit hit;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
+            int mask = 1 << LayerMask.NameToLayer(excluseLayeName) | layerMask.value;
+
+            if(Physics.Raycast(transform.position,fwd,out hit, rayLength, mask))
+            {
+               
+                raycastObject = hit.collider.gameObject;
+                if (raycastObject.CompareTag("Door"))
+                {
+                    if(Input.GetKeyDown(KeyCode.T))
+                        raycastObject.GetComponentInParent<GateOpen>().toggleGate();
+                }
+            }
+        }
     }
 
 }
