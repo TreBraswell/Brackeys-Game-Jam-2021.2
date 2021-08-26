3f89d153e68b8aec6417f0dd47ca539c23f2fd30
Abhishek Singh <abhisheksingh360degree@gmail.com>
2021-08-26 08:31:33 +0530

    Upload

--- Brackeys Game Jam 2021.2/Assets/_/Work - Naron/Scripts/Playables/PlayerMove.cs
+++ Brackeys Game Jam 2021.2/Assets/_/Work - Naron/Scripts/Playables/PlayerMove.cs
@@ -6,59 +6,46 @@ namespace BGJ20212.Game.Naron
 
     public class PlayerMove : MonoBehaviour
     {
-
         //Refs
         [Header("Refrences")] [SerializeField] private PlayerAnimator animator;
-        private Rigidbody rb;
-        [SerializeField] private LayerMask groundLayer;
-        [SerializeField] private Collider collider;
+
+        [SerializeField] private CharacterController characterController;
+
+        public float gravity = -9.81f;
+        Vector3 velocity;
 
         private Transform cameraTransform;
 
+        public Transform GroundCheckPos;
+        public float radius;
+        public LayerMask whatIsGround;
+
         //Stats
         [Header("Stats")] public float move_Speed = 250f;
-        public float run_Speed = 400f;
         public float speed;
         public float jumpHeight;
 
         private bool isGrounded;
-        private bool isJumping;
         private bool isAttacking;
         private bool canAttack;
 
         private bool isStanding;
-        private bool isSpriting;
-
-
-
-        //For raycast, can be deleted later
-        [SerializeField] private float playerHeight;
-
-
-
 
         //private stuff for physisc
         private Vector3 direction;
-        [Header("Drag")] public float gorundDrag = 6f;
-        public float airDrag = 2f;
+
         private float turnSmoothTime = 0.2f;
         private float turnSmoothVelocity;
-
-
 
         // Start is called before the first frame update
         void Start()
         {
             cameraTransform = Camera.main.transform;
-            rb = GetComponent<Rigidbody>();
-            collider = GetComponent<Collider>();
-            playerHeight = collider.bounds.size.y;
+            characterController = GetComponent<CharacterController>();
 
             speed = move_Speed;
 
-
             canAttack = true;
-
         }
 
         private void Update()
@@ -68,11 +55,13 @@ namespace BGJ20212.Game.Naron
 
         // Update is called once per frame
         void FixedUpdate()
-
         {
-            DragControl();
             Move();
             GroundCheck();
+            if(isGrounded && Input.GetButtonDown(Axis.JUMP))
+            {
+                Jumping();
+            }
         }
 
         #region Movement
@@ -86,129 +75,53 @@ namespace BGJ20212.Game.Naron
             else
             {
                 animator.SetBool("Move", true);
+                velocity.y = -2f;
             }
 
-            Vector2 inputAxis = new Vector2(Input.GetAxis(Axis.HORIZONTAL), Input.GetAxis(Axis.VERTICAL));
+            Vector2 inputAxis = new Vector2(Input.GetAxis(Axis.HORIZONTAL), Mathf.Clamp(Input.GetAxis(Axis.VERTICAL), 0f, 1f));
 
             direction = new Vector3(inputAxis.x, 0f, inputAxis.y).normalized;
 
             if (direction.magnitude >= 0.1f)
             {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +
-                                    cameraTransform.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
-                    turnSmoothTime);
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
                 Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                rb.velocity = new Vector3(moveDirection.x * speed, rb.velocity.y, moveDirection.z * speed);
-            }
-
-
-            //Max speed
-            float speedPercent = rb.velocity.sqrMagnitude / run_Speed / run_Speed;
-            animator.SetFloat("moveSpeed", speedPercent);
-
-
-
-        }
-
-        void DragControl()
-        {
-            if (isGrounded)
-            {
-
-                rb.drag = gorundDrag;
+                characterController.Move(new Vector3(moveDirection.x * speed, 0f, moveDirection.z * speed));
+                animator.SetFloat("moveSpeed", 40f);
 
             }
+
             else
             {
-                rb.drag = airDrag;
+                animator.SetFloat("moveSpeed", 0f);
             }
 
-        }
-
-        private void WalkAndSprint()
-        {
-            if (isSpriting)
-            {
-                isSpriting = false;
-                speed = move_Speed;
-            }
-            else
-            {
-
-                isSpriting = true;
-                speed = run_Speed;
-
-            }
+            velocity.y += gravity * Time.deltaTime;
+            characterController.Move(velocity * Time.deltaTime);
         }
 
         #endregion
 
         private void CheckInput()
         {
-            if (Input.GetButtonDown(Axis.JUMP))
-            {
-                TryJump();
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                WalkAndSprint();
-            }
-
             if (Input.GetMouseButtonDown(0))
             {
-
                 Attack();
             }
+        }
 
-            if (Input.GetKeyDown(KeyCode.O))
-            {
-                ToggleStand();
-            }
-
+        void Jumping()
+        {
+            animator.SetBool("Jump", true);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         private void GroundCheck()
         {
-            isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2f + 0.1f, groundLayer);
-        }
-
-        private void TryJump()
-        {
-
-
-
-            if (isGrounded && !isAttacking && !isStanding)
-            {
-                if (isJumping)
-                {
-                    isJumping = false;
-                }
-
-                //Animation
-                animator.SetBool("Jump", true);
-
-                Invoke("Jump", 0.3f);
-                //Add delay
-
-
-
-            }
-
-
-        }
-
-        private void Jump()
-        {
-
-
-
-            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
-            isJumping = true;
-
+            isGrounded = Physics.CheckSphere(GroundCheckPos.position, radius, whatIsGround);
         }
 
         #region attack
@@ -234,9 +147,6 @@ namespace BGJ20212.Game.Naron
         }
 
         #endregion
-
-
-
 
         void ToggleStand()
         {
