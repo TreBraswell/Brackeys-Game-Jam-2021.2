2f589752a2ba248d8db9bf883aa12cbf84c8de1a
Abhishek Singh <abhisheksingh360degree@gmail.com>
2021-08-26 16:36:54 +0530

    Audio Manager and Nav Mesh Added
    Audio Manager and Nav Mesh Added

---
+++ Brackeys Game Jam 2021.2/Assets/_/Work - Abhi/Meshes/AudioManager.cs
@@ -0,0 +1,52 @@
+namespace BGJ20212.Game.AbhiTechGame
+{
+    using UnityEngine.Audio;
+    using UnityEngine;
+    using System;
+
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] Sound;
+        public static AudioManager instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+
+            foreach (Sound s in Sound)
+            {
+                s.source = gameObject.AddComponent<AudioSource>();
+
+                s.source.outputAudioMixerGroup = s.Output;
+
+                s.source.clip = s.audioClip;
+
+                s.source.volume = s.Volume;
+
+                s.source.pitch = s.Pitch;
+
+                s.source.loop = s.loop;
+            }
+        }
+
+        private void Start()
+        {
+            Play("Wind Blow");
+            Play("STM Back");
+        }
+
+        public void Play(string AudioName)
+        {
+            Sound s = Array.Find(Sound, sound => sound.Name == AudioName);
+            if(s != null)
+                s.source.Play();
+        }
+    }
+}
---
+++ Brackeys Game Jam 2021.2/Assets/_/Work - Abhi/Meshes/Sound.cs
@@ -0,0 +1,28 @@
+namespace BGJ20212.Game.AbhiTechGame
+{
+
+    using UnityEngine.Audio;
+    using UnityEngine;
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string Name;
+
+        public AudioMixerGroup Output;
+
+        public AudioClip audioClip;
+
+        [Range(0f, 1f)]
+        public float Volume = 1f;
+
+        [Range(.1f, 3f)]
+        public float Pitch = 1f;
+
+        public bool loop;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+    
+}
