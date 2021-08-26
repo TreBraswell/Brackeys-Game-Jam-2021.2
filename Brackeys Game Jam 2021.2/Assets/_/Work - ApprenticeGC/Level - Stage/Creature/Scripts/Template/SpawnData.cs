ea9a7946a92e7147b3889210be47ecca010d8d68
ApprenticeGC <apprentice@giantcroissant.com>
2021-08-26 20:08:25 +0800

    Add bar for hp ui
    Add initial bolt graph to handle spawning, still WIP

---
+++ Brackeys Game Jam 2021.2/Assets/_/Work - ApprenticeGC/Level - Stage/Creature/Scripts/Template/SpawnData.cs
@@ -0,0 +1,18 @@
+﻿namespace BGJ20212.Game.ApprenticeGC.Dialogue.Template
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [System.Serializable]
+    public class SpawnLocation
+    {
+        public Vector3 position;
+        public GameObject creaturePrefab;
+    }
+
+    [CreateAssetMenu(fileName = "SpawnData", menuName = "BGJ20212/Creature/SpawnData")]
+    public class SpawnData : ScriptableObject
+    {
+        public List<SpawnLocation> spawnLocations;
+    }
+}
