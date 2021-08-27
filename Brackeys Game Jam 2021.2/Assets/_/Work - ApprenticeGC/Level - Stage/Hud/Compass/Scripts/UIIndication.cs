ebb4ca87b4038c587348833818120d88126e798d
Abhishek Singh <abhisheksingh360degree@gmail.com>
2021-08-27 20:28:14 +0530

    Revert "Create compass script for showing indication"
    This reverts commit e896e1e2d521fe6bca0ba0936a4c994874cbf66d.

--- Brackeys Game Jam 2021.2/Assets/_/Work - ApprenticeGC/Level - Stage/Hud/Compass/Scripts/UIIndication.cs
+++ Brackeys Game Jam 2021.2/Assets/_/Work - ApprenticeGC/Level - Stage/Hud/Compass/Scripts/UIIndication.cs
@@ -3,27 +3,11 @@ namespace BGJ20212.Game.ApprenticeGC.Hud
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
-    using UnityEngine.UI;
 
-    public class UIIndication :
-        MonoBehaviour//,
-        //MyPooler.IPooledObject
+    public class UIIndication : MonoBehaviour
     {
         public Sprite icon;
-        public Image image;
 
         public Vector2 MappedPos => new Vector2(transform.position.x, transform.position.z);
-
-        // public void OnRequestedFromPool()
-        // {
-        //     //
-        //     icon = null;
-        //     image = null;
-        // }
-        //
-        // public void DiscardToPool()
-        // {
-        //     MyPooler.ObjectPooler.Instance.ReturnToPool("Indication", this.gameObject);
-        // }
     }
 }
