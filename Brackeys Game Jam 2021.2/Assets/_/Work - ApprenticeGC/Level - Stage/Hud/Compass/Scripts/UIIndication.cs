a9a419bcc6688bd4d168faa464da91b316905226
Abhishek Singh <abhisheksingh360degree@gmail.com>
2021-08-28 06:19:19 +0530

    Upload 2

--- Brackeys Game Jam 2021.2/Assets/_/Work - ApprenticeGC/Level - Stage/Hud/Compass/Scripts/UIIndication.cs
+++ Brackeys Game Jam 2021.2/Assets/_/Work - ApprenticeGC/Level - Stage/Hud/Compass/Scripts/UIIndication.cs
@@ -3,11 +3,27 @@ namespace BGJ20212.Game.ApprenticeGC.Hud
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.UI;
 
-    public class UIIndication : MonoBehaviour
+    public class UIIndication :
+        MonoBehaviour//,
+        //MyPooler.IPooledObject
     {
         public Sprite icon;
+        public Image image;
 
         public Vector2 MappedPos => new Vector2(transform.position.x, transform.position.z);
+
+        // public void OnRequestedFromPool()
+        // {
+        //     //
+        //     icon = null;
+        //     image = null;
+        // }
+        //
+        // public void DiscardToPool()
+        // {
+        //     MyPooler.ObjectPooler.Instance.ReturnToPool("Indication", this.gameObject);
+        // }
     }
 }
