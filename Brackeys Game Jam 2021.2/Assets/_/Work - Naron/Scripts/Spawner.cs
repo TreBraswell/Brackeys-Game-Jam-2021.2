d858e5201402fd24db75e74085a35d068d8a6e98
NaronB66 <narek660592@gmail.com>
2021-08-28 23:33:18 -0600

    push

---
+++ Brackeys Game Jam 2021.2/Assets/_/Work - Naron/Scripts/Spawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BGJ20212.Game.Naron
+{
+    public class Spawner : MonoBehaviour
+    {
+        enemyOptions[] spawnPoint;
+        private int waveNumber = 0;
+        private int enemySpawnAmount = 0;
+        public int enemyKilled = 0;
+
+        /*
+        private void Start()
+        {
+            spawners = new GameObject[spawnPoints];
+            for (int i = 0; i < spawnPoints; i++)
+            {
+                spawners[i] = transform.GetChild(i).gameObject;
+            }
+            StartWave();
+        }
+        private void Update()
+        {
+    
+        }
+        private void SpawnEnemy()
+        {
+            int randSpawnPlace = Random.Range(0, spawnPoints);
+            Instantiate(enemyPrefab, spawners[randSpawnPlace].transform.position, Quaternion.identity);
+        }
+        private void StartWave()
+        {
+            waveNumber = 1;
+            enemySpawnAmount = 2;
+            enemyKilled = 0;
+            for (int i = 0; i < enemySpawnAmount; i++)
+            {
+                SpawnEnemy();
+            }
+        }
+        private void NextWave()
+        {
+            waveNumber++;
+            enemySpawnAmount += 2;
+            enemyKilled = 0;
+        }
+    }
+        */
+        [System.Serializable]
+        struct enemyOptions
+        {
+            [SerializeField] private int spawnPoints;
+
+            [SerializeField] private GameObject enemyPrefab;
+        }
+    }
+}
